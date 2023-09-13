616c2f0a1b16459546f9bf85a55896045c76777b
kamangir <arash@kamangir.net>
2023-09-12 20:18:16 -0700

    first image - kamangir/bolt#746

--- .abcli/install/hubble.sh
+++ .abcli/install/hubble.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
 function abcli_install_blue_plugin() {
-    abcli_log "Installing..."
+    # https://docs.astropy.org/en/stable/install.html
+    pip3 install astropy[recommended]
 }
 
-abcli_install_module blue_plugin 101
+abcli_install_module blue_plugin 102
--- hubble/__init__.py
+++ hubble/__init__.py
@@ -1,5 +1,5 @@
 NAME = "hubble"
 
-VERSION = "3.9.1"
+VERSION = "3.10.1"
 
 from .functions import *
