376bc3d275cf46f3b1a9e8eca1f65efe1431dcf5
kamangir <arash@kamangir.net>
2024-03-04 00:22:23 -0800

    test refactor - kamangir/bolt#1234

--- hubble/__init__.py
+++ hubble/__init__.py
@@ -4,4 +4,4 @@ ICON = "🔭"
 
 DESCRIPTION = f"{ICON} tools to access and process Hubble Space Telescope imagery and other datasets on AWS Open Data Registry."
 
-VERSION = "3.302.1"
+VERSION = "3.303.1"
