e4f93f657f1cfc32aefb089e010d222e680ca42c
kamangir <arash@kamangir.net>
2024-04-21 18:17:20 -0700

    workflow fix (hopefully) - kamangir/bolt#746

--- .abcli/install/hubble.sh
+++ .abcli/install/hubble.sh
@@ -1,10 +1,7 @@
 #! /usr/bin/env bash
 
 function abcli_install_hubble() {
-    # https://docs.astropy.org/en/stable/install.html
-    pip3 install astropy[recommended]
-
-    abcli_git clone git@github.com:awslabs/open-data-registry.git
+    abcli_git clone https://github.com/awslabs/open-data-registry.git
 }
 
 abcli_install_module hubble 104
