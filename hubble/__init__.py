a36671f8f495c148b3bae86ae915a4c5ea8c8b8d
kamangir <arash@kamangir.net>
2024-04-21 18:09:20 -0700

    workflow fix (hopefully) - kamangir/bolt#746

--- hubble/__init__.py
+++ hubble/__init__.py
@@ -4,5 +4,4 @@ ICON = "🔭"
 
 DESCRIPTION = f"{ICON} tools to access and process Hubble Space Telescope imagery and other datasets on AWS Open Data Registry."
 
-VERSION = "3.308.1"
-
+VERSION = "3.309.1"
