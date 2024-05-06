5302398c23c258ed0473a0d75e93b18d3e5e1158
kamangir <arash@kamangir.net>
2024-05-05 21:30:13 -0700

    @ls refactor - kamangir/bolt#746

--- .abcli/list.sh
+++ .abcli/list.sh
@@ -7,14 +7,14 @@ function abcli_hubble_list() {
     local task=$1
 
     if [ "$task" == help ]; then
-        abcli_show_usage "hubble list dataset$ABCUL$EOP.|<hubble-dataset-name>$EOPE" \
-            "list <hubble-dataset-name>, example: hst."
+        abcli_show_usage "hubble list dataset$ABCUL$EOP.|<hubble-dataset-name>$ABCUL<suffix>$EOPE" \
+            "list <hubble-dataset-name>/<suffix>, example: hst."
 
         local options="keyword=<keyword>"
         abcli_show_usage "hubble list datasets$ABCUL$EOP$options$EOPE" \
             "list hubble datasets."
 
-        abcli_show_usage "hubble list ${EOP}object$ABCUL.|<hubble-object-name>$EOPE" \
+        abcli_show_usage "hubble list ${EOP}object$ABCUL.|<hubble-object-name>$ABCUL<suffix>$EOPE" \
             "list <hubble-object-name> in $abcli_hubble_dataset_name, example in hst: public/u4ge/u4ge0106r."
         return
     fi
@@ -36,39 +36,39 @@ function abcli_hubble_list() {
         return 0
     fi
 
+    local suffix
     if [[ -z "$thing_type" ]]; then
-        local thing_type=object
-        local thing_name=$abcli_hubble_object_name
-        local args="${@:2}"
+        thing_type=object
+        thing_name=$abcli_hubble_object_name
+        suffix="${@:2}"
     elif [[ $(abcli_hubble_is_dataset $thing_type) == 1 ]]; then
-        local thing_type=dataset
-        local thing_name=${1:-.}
-        local args="${@:2}"
+        thing_type=dataset
+        thing_name=${1:-.}
+        suffix="${@:2}"
     elif [[ "$thing_type" != dataset ]] && [[ "$thing_type" != object ]]; then
-        local thing_type=object
-        local thing_name=${1:-.}
-        local args="${@:2}"
+        thing_type=object
+        thing_name=${1:-.}
+        suffix="${@:2}"
     fi
 
     local dataset_name=$abcli_hubble_dataset_name
     [[ "$thing_type" == dataset ]] &&
-        local dataset_name=$(abcli_clarify_object "$thing_name" "" hubble_dataset)
+        dataset_name=$(abcli_clarify_object "$thing_name" "" hubble_dataset)
 
     local object_name=""
     [[ "$thing_type" == object ]] &&
-        local object_name=$(abcli_clarify_object "$thing_name" "" hubble_object)
+        object_name=$(abcli_clarify_object "$thing_name" "" hubble_object)
 
     abcli_log "🔭 $dataset_name :: $object_name"
 
     local s3_uri=$(abcli_hubble_get s3_uri $dataset_name $object_name)
+    [[ ! -z "$suffix" ]] && s3_uri=$s3_uri$suffix
     abcli_log "🔗 $s3_uri"
 
     # https://registry.opendata.aws/hst/
-    local command_line="aws s3 ls \
+    abcli_eval - \
+        "aws s3 ls \
         $(abcli_hubble_get auth $dataset_name) \
         $s3_uri \
         $args"
-
-    abcli_log "⚙️  $command_line"
-    eval "$command_line"
 }
