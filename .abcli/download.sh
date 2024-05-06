2d339f496773419c716d440f8edbc161a8b959f3
kamangir <arash@kamangir.net>
2024-05-05 20:52:51 -0700

    download refactor - kamangir/bolt#746

--- .abcli/download.sh
+++ .abcli/download.sh
@@ -4,7 +4,7 @@ function abcli_hubble_download() {
     local options=$1
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        local options="~dryrun,filename=<filename>|all,~ingest,upload"
+        local options="~dryrun,filename=<filename>|all,~ingest,upload,verbose"
         abcli_show_usage "hubble download$ABCUL[$options]$ABCUL[.|<hubble-object-name>]$ABCUL[.|<object-name>]" \
             "$abcli_hubble_dataset_name/<hubble-object-name> -> <object-name>."
         return
@@ -16,6 +16,7 @@ function abcli_hubble_download() {
     local do_dryrun=$(abcli_option_int "$options" dryrun 1)
     local do_upload=$(abcli_option_int "$options" upload 0)
     local do_ingest=$(abcli_option_int "$options" ingest 1)
+    local do_verbose=$(abcli_option_int "$options" verbose 0)
 
     local hubble_object_name=$(abcli_clarify_object "$2" . hubble_object)
     local object_name=$(abcli_clarify_object "$3" .)
@@ -23,17 +24,21 @@ function abcli_hubble_download() {
     local auth=$(abcli_hubble_get auth $dataset_name)
     local s3_uri=$(abcli_hubble_get s3_uri $dataset_name $hubble_object_name)
 
+    local command_line
     if [ "$filename" == all ]; then
-        local command_line="aws s3 sync \
+        command_line="aws s3 sync \
             $auth \
             $s3_uri \
             $abcli_object_root/$object_name/"
     else
-        local command_line="aws s3 cp \
+        command_line="aws s3 cp \
             $auth \
             $s3_uri$filename \
             $abcli_object_root/$object_name/"
     fi
+
+    [[ "$do_verbose" == 0 ]] &&
+        command_line="$command_line --only-show-errors"
 
     abcli_eval dryrun=$do_dryrun \
         "$command_line"
