3098632e3210d358a0e8802fbd0bd91a9ee90b63
kamangir <arash@kamangir.net>
2024-05-24 18:55:18 -0700

    refactor - kamangir/bolt#746

--- .abcli/install/hubble.sh
+++ .abcli/install/hubble.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-function abcli_install_hubble() {
+function abcli_install_open_data_registry() {
     abcli_git clone https://github.com/awslabs/open-data-registry.git
 }
 
-abcli_install_module hubble 104
+abcli_install_module open_data_registry 201
