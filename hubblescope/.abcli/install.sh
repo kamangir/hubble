3dfff2d18c4052b732f0f166fc7257bbdff506cb
kamangir <arash@kamangir.net>
2024-05-26 14:56:09 -0700

    refactor - kamangir/bolt#746

--- hubblescope/.abcli/install.sh
+++ hubblescope/.abcli/install.sh
@@ -4,4 +4,4 @@ function abcli_install_open_data_registry() {
     abcli_git clone https://github.com/awslabs/open-data-registry.git
 }
 
-abcli_install_module open_data_registry 201
+abcli_install_module open_data_registry 3.1.1
