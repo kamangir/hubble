8de206204d176dd61087cc63ef33d6ed119f92ad
kamangir <arash@kamangir.net>
2024-10-13 13:17:51 -0700

    git refactor - kamangir/bolt#746

--- hubblescope/.abcli/actions.sh
+++ hubblescope/.abcli/actions.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 function hubble_action_git_before_push() {
-    [[ "$(abcli_git get_branch)" == "main" ]] &&
-        hubble pypi build
+    [[ "$(abcli_git get_branch)" != "main" ]] &&
+        return 0
+
+    hubble pypi build
 }
